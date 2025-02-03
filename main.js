2ad0eb124f92b1ef9b6760e6ceffb8881b39a2fe
mu <shavkatovmurod2004@gmail.com>
2025-02-03 18:17:44 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -55,3 +55,92 @@ new Chart(ctx3, {
         responsive: true
     }
 });
+
+let ctx4 = document.getElementById('pieChart1').getContext('2d');
+new Chart(ctx4, {
+    type: 'radar',
+    data: {
+        labels: ['Apple', 'Samsung', 'Huawei', 'Xiaomi'],
+        datasets: [{
+            label: 'Market Share',
+            data: [30, 25, 20, 25],
+            backgroundColor: ['red', 'blue', 'yellow', 'green']
+        }]
+    },
+    options: {
+        responsive: true
+    }
+});
+
+let ctx5 = document.getElementById('pieChart2').getContext('2d');
+new Chart(ctx5, {
+    type: 'doughnut', // 'bar', 'line', 'pie', 'doughnut', 'radar', etc.
+    data: {
+        labels: ['Yanvar', 'Fevral', 'Mart', 'Aprel', 'May', 'Iyun'],
+        datasets: [
+            {
+                label: 'Sales',
+                data: [12, 19, 3, 5, 2, 7],
+                backgroundColor: ['rgba(75, 192, 192, 0.6)', 'red', 'aqua', 'orange', 'blue'],
+                borderColor: 'rgba(75, 192, 192, 1)',
+                borderWidth: 2,
+            },
+        ],
+    },
+    options: {
+        responsive: true,
+        plugins: {
+            legend: {
+                display: true,
+                position: 'top',
+            },
+        },
+        scales: {
+            y: {
+                beginAtZero: true,
+            },
+        },
+    },
+})
+
+
+let ctx6 = document.getElementById('pieChart3').getContext('2d');
+new Chart(ctx6, {
+    type: 'polarArea', // 'bar', 'line', 'pie', 'polarArea', 'doughnut', etc.
+    data: {
+        labels: ['Apple', 'Samsung', 'Huawei', 'Xiaomi'],
+        datasets: [
+            {
+                label: 'Market Share',
+                data: [30, 25, 20, 25],
+                backgroundColor: ['rgba(75, 192, 192, 0.6)', 'red', 'aqua', 'orange'],
+                borderColor: 'rgba(75, 192, 192, 1)',
+                borderWidth: 2,
+            },
+        ],
+    },
+    options: {
+        responsive: true,
+        plugins: {
+            legend: {
+                display: true,
+                position: 'top',
+            },
+            tooltip: {
+                enabled: true,
+                mode: 'index',
+                intersect: false,
+            },
+            title: {
+                display: true,
+                text: 'Market Share',
+            },
+        },
+    },
+    scales: {
+        y: {
+            beginAtZero: true,
+        },
+    },
+},
+)
